0674631118a7f0a41a84280a6a23f54e19a01676
luispieta <lufelipepieta@gmail.com>
2026-02-26 08:30:40 -0300

    Feat: - Rotas aninhadas - Melhoria na estrutura do código, utilizando boas práticas nas Rotas

--- src/paginas/SobreMim/index.jsx
+++ src/paginas/SobreMim/index.jsx
@@ -1,10 +1,5 @@
-import Banner from "../../componentes/Banner"
-
 export default function SobreMim() {
     return(
-        <main>
-            <Banner/>
-            <h1>Olá mundo</h1>
-        </main>
+        <h1>Olá mundo</h1>
     )
 }
